cebb6a297e540050e2ff819a4d917613435f8419
Tixtex <tixtex666@gmail.com>
2022-07-31 16:39:14 +0200

    move to wsl

--- libftprintf.c
+++ libftprintf.c
@@ -1,6 +1,50 @@
-#include "libftprintf.h"
+#include "./libftprintf.h"
 
-int		ft_printf(const char*str, ...)
+static void	pf_char_handler(t_data *d)
 {
+	register size_t	size;
+	register char	*start;
 
+	size = ZERO;
+	start = d->to_print;
+	while ('%' != *d->to_print && '\0' != *d->to_print)
+	{
+		size++;
+		d->chars_printed++;
+		d->to_print++;
+	}
+	if (ZERO != size)
+		write(STDOUT_FILENO, start, size * sizeof(const char));
+	if ('%' == *d->to_print)
+	{
+		d->to_print++;
+		pf_flag_handler(d);
+	}
+}
+
+static void	pf_init(t_data *data)
+{
+	data->chars_printed = ZERO;
+	data->flag.neg = -1;
+	data->flag.zero = -1;
+	data->flag.point = -1;
+	data->flag.width = -1;
+	data->flag.hastag = -1;
+	data->flag.space = -1;
+	data->flag.plus = -1;
+}
+
+int	ft_printf(const char *str, ...)
+{
+	t_data	d;
+
+	if (NULL == str)
+		return (ZERO);
+	pf_init(&d);
+	d.to_print = str;
+	va_start(d.ap, str);
+	while ('\0' != *d.to_print) 
+		pf_char_handler(&d);
+	va_end(d.ap);
+	return (d.chars_printed);
 }
--- libftprintf.h
+++ libftprintf.h
@@ -2,14 +2,32 @@
 # define LIBFTPRINTF_H
 # include "libft/libft.h"
 # include <stdarg.h>
+# ifndef FLAGS_M
+#  define FLAGS_M "0123456789-*.#+ "
+# endif
+typedef struct s_flags
+{
+	int			neg;
+	int			zero;
+	int			point;
+	int			width;
+	int			hastag;
+	int			space;
+	int			plus;
+}				t_flags;
+
 
 typedef struct s_data
 {
-	vartype		varname;
+	int			chars_printed;
+	const char	*to_print;
+	t_flags		flag;
+	va_list		ap;
 }				t_data;
 
 /**	libftprintf	**/
-int		ft_printf(const char*str, ...);
+int		ft_printf(const char *str, ...);
 /**	sources	**/
+void	pf_flag_handler(t_data *data);
 
 #endif
